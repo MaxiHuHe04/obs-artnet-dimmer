0c1d0cd7524a4a5349f68edab0ebd4090872e62e
MaxiHuHe04 <maxiherczegh@outlook.com>
2022-08-27 22:11:34 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+export const config = {
+    SHORT_NAME: 'OBS',
+    LONG_NAME: 'OBS Dimmer',
+    UNIVERSE: 0,
+    NET: 0,
+    SUBNET: 0,
+    CHANNEL: 0,  // 0-511
+    OBS_WS_URL: 'ws://localhost:4455',
+    OBS_PASSWORD: null,
+    OBS_SOURCE_NAME: 'Black Overlay',
+    OPACITY_FILTER_NAME: 'Opacity',
+    BRIGHTNESS_POWER: 1
+}
---
+++ index.js
@@ -0,0 +1,50 @@
+import OBSWebSocket from "obs-websocket-js";
+import {dmxnet} from "dmxnet";
+import {config} from "./config.js";
+
+const artNet = new dmxnet({sName: config.SHORT_NAME, lName: config.LONG_NAME})
+const receiver = artNet.newReceiver({universe: config.UNIVERSE, net: config.NET, subnet: config.SUBNET})
+
+const obs = new OBSWebSocket();
+
+async function createOpacityFilterIfMissing() {
+    const {filters} = await obs.call("GetSourceFilterList", {sourceName: config.OBS_SOURCE_NAME});
+    const opacityFilter = filters.find(value => value['filterName'] === config.OPACITY_FILTER_NAME);
+    if (opacityFilter != null) {
+        if (opacityFilter.filterKind !== 'color_filter_v2') {
+            console.warn(`Filter '${opacityFilter.filterName}' has the wrong type (${opacityFilter.filterKind} instead of color_filter_v2)`)
+        }
+        return;
+    }
+
+    await obs.call('CreateSourceFilter', {
+        sourceName: config.OBS_SOURCE_NAME,
+        filterName: config.OPACITY_FILTER_NAME,
+        filterKind: 'color_filter_v2'
+    });
+}
+
+try {
+    await obs.connect(config.OBS_WS_URL, config.OBS_PASSWORD).then(() => console.log('Connection to OBS successful'));
+} catch (error) {
+    console.error('Failed to connect to OBS:', error.code, error.message);
+    process.exit(1);
+}
+
+try {
+    await createOpacityFilterIfMissing();
+} catch (error) {
+    console.error(error);
+    process.exit(1);
+}
+
+receiver.on('data', data => {
+    if (data.length < config.CHANNEL + 1) return;
+    const brightness = 1 - (data[config.CHANNEL] / 255) ** config.BRIGHTNESS_POWER;
+    obs.call('SetSourceFilterSettings', {
+        sourceName: config.OBS_SOURCE_NAME,
+        filterName: config.OPACITY_FILTER_NAME,
+        filterSettings: {opacity: brightness}
+    }).catch(() => createOpacityFilterIfMissing())
+        .catch(err => console.warn(err))
+});
